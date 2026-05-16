712e38184d91f172e1f0f8748bf61453145e830a
Cecep Nandang <cecep@kima.one>
2026-05-16 19:52:09 +0700

    fix: bug

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,5 @@
-const backendUrl = (import.meta.env.VITE_API_URL || 'http://localhost:5000').replace(/\/$/, '');
+// const backendUrl = (import.meta.env.VITE_API_URL || 'http://localhost:5000').replace(/\/$/, '');
+const backendUrl = "https://api-inventory.isavralabel.com/visa-arabic";
 
 export const BACKEND_URL = backendUrl;
 export const API_BASE_URL = `${backendUrl}/api`;
